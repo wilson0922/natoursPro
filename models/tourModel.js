4c04125d16dd4513487904e85201cd39ca5b99c4
tree70373 <tree70373@gmail.com>
2023-06-27 02:49:56 +0800

    custom validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,14 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name.'],
       unique: true,
       trim: true,
+      maxlength: [
+        40,
+        'A tour name must have less or equal than 40 characters.',
+      ],
+      minlength: [
+        10,
+        'A tour name must have more or equal than 10 characters.',
+      ],
     },
     slug: String,
     duration: {
@@ -21,10 +29,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty.'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult.',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0.'],
+      max: [5, 'Rating must be below 5.0.'],
     },
     ratingsQuantity: {
       type: Number,
@@ -36,6 +50,13 @@ const tourSchema = new mongoose.Schema(
     },
     priceDiscount: {
       type: Number,
+      // this only points to current doc on NEW
+      validate: {
+        validator: function (val) {
+          return val < this.price
+        },
+        message: 'Discount price ({VALUE}) should be below the regular price.',
+      },
     },
     summary: {
       type: String,
