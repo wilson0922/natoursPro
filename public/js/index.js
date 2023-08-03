516543b6caaae24787bb297eb9d0645ef4ca6224
tree70373 <tree70373@gmail.com>
2023-08-03 08:38:03 +0800

    add stripe payment functions

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 import { login, logout } from './login'
 import { displayMap } from './mapbox'
 import { updateSettings } from './updateSettings'
+import { bookTour } from './stripe'
 
 window.onbeforeunload = function () {
   window.scrollTo(0, 0)
@@ -11,6 +12,7 @@ const loginForm = document.querySelector('.form--login')
 const logOutBtn = document.querySelector('.nav__el--logout')
 const userDataForm = document.querySelector('.form-user-data')
 const userPasswordForm = document.querySelector('.form-user-password')
+const bookBtn = document.getElementById('book-tour')
 
 // Delegation
 if (mapBox) {
@@ -54,4 +56,11 @@ if (userPasswordForm)
     document.getElementById('password-current').value = ''
     document.getElementById('password').value = ''
     document.getElementById('password-confirm').value = ''
+  })
+
+if (bookBtn)
+  bookBtn.addEventListener('click', e => {
+    e.target.textContent = 'Processing...'
+    const { tourId } = e.target.dataset
+    bookTour(tourId)
   })
---
+++ public/js/stripe.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import { showAlert } from './alerts'
+const Stripe = require('stripe')
+const stripe = Stripe(
+  'pk_test_51N60JzCKQnZMBL36F2mHQmTiuK1jDJTFklP6REPVNSD4e5q5mAAXqHsDPwGnZQr0IJd4d82q7s5X7iokObb1Hmcs00p9tdsbOj'
+)
+
+export const bookTour = async tourId => {
+  try {
+    // 1) Get checkout session
+    const session = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+    )
+    // 2) Create checkout form + charge credit card
+    window.location.assign(session.data.session.url)
+  } catch (err) {
+    console.log(err)
+    showAlert('err', err)
+  }
+}
