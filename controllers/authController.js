6a349e62b9eba9e518850f598c7446731c6c79c5
tree70373 <tree70373@gmail.com>
2023-08-03 16:53:24 +0800

    add deployment config

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,16 +14,15 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id)
-  const cookieOptions = {
+
+  res.cookie('jwt', token, {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-  }
-
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true
-
-  res.cookie('jwt', token, cookieOptions)
+    secure: (req.secure =
+      true || req.headers(x - forwarded - proto) === 'https'),
+  })
 
   // Remove password from output
   user.password = undefined
