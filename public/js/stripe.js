2ce8a343dca236d43162e2aaa640b755fd58951f
tree70373 <tree70373@gmail.com>
2023-08-04 19:27:40 +0800

    fix bookTour url

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -8,7 +8,7 @@ const stripe = Stripe(
 export const bookTour = async tourId => {
   try {
     // 1) Get checkout session
-    const session = await axios(`/bookings/checkout-session/${tourId}`)
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`)
     // 2) Create checkout form + charge credit card
     window.location.assign(session.data.session.url)
   } catch (err) {
