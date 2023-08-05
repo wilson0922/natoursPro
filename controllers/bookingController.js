e68c73421e7ab5c2b1e1505d9d236ec615bb6207
tree70373 <tree70373@gmail.com>
2023-08-05 15:02:44 +0800

    fix webhook

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -9,39 +9,44 @@ const factory = require('./handlerFactory')
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get currntly booked tour
   const tour = await Tour.findById(req.params.tourId)
+
   // 2) Create checkout session
-  const session = await stripe.checkout.sessions.create({
-    mode: 'payment',
-    invoice_creation: {
-      enabled: true,
-    },
-    payment_method_types: ['card'],
-    // success_url: `${req.protocol}://${req.get('host')}/?tour=${
-    //   req.params.tourId
-    // }&user=${req.user.id}&price=${tour.price}`,
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
-    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
-    customer_email: req.user.email,
-    client_reference_id: req.params.tourId,
-    line_items: [
-      {
-        quantity: 1,
-        price_data: {
-          currency: 'usd',
-          unit_amount: tour.price * 100,
-          product_data: {
-            name: `${tour.name} Tour`,
-            description: tour.summary,
-            images: [
-              `${req.protocol}://${req.get('host')}/img/tours/${
-                tour.imageCover
-              }`,
-            ],
+  let session
+
+  try {
+    session = await stripe.checkout.sessions.create({
+      mode: 'payment',
+      payment_method_types: ['card'],
+      // success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      //   req.params.tourId
+      // }&user=${req.user.id}&price=${tour.price}`,
+      success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+      cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+      customer_email: req.user.email,
+      client_reference_id: req.params.tourId,
+      line_items: [
+        {
+          quantity: 1,
+          price_data: {
+            currency: 'usd',
+            unit_amount: tour.price * 100,
+            product_data: {
+              name: `${tour.name} Tour`,
+              description: tour.summary,
+              images: [
+                `${req.protocol}://${req.get('host')}/img/tours/${
+                  tour.imageCover
+                }`,
+              ],
+            },
           },
         },
-      },
-    ],
-  })
+      ],
+    })
+  } catch (err) {
+    console.log(err)
+  }
+
   // 3) Create session as response
   res.status(200).json({
     status: 'success',
